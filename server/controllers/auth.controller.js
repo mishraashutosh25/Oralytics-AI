fb663d7d1a4bf0376cb34df5f117ab18a2bcf57d
ASHUTOSH MISHRA <168020481+mishraashutosh25@users.noreply.github.com>
2026-03-31 16:15:33 +0530

    Update cookie settings for secure authentication

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -18,8 +18,8 @@ export const googleAuth = async (req, res) => {
 
     res.cookie("token", token, {
       httpOnly: true,
-      secure: false,
-      sameSite: "lax",
+      secure: true,
+      sameSite: "None",
       maxAge: 7 * 24 * 60 * 60 * 1000
     });
 
@@ -47,8 +47,8 @@ export const githubAuth = async (req, res) => {
 
     res.cookie("token", token, {
       httpOnly: true,
-      secure: false,
-      sameSite: "strict",
+      secure: true,
+      sameSite: "None",
       maxAge: 7 * 24 * 60 * 60 * 1000
     });
 
@@ -61,7 +61,11 @@ export const githubAuth = async (req, res) => {
 
 export const logOut = async (req, res) => {
   try {
-    res.clearCookie("token");
+   res.clearCookie("token", {
+  httpOnly: true,
+  secure: true,
+  sameSite: "None"
+});
 
     return res.status(200).json({
       message: "Logged out successfully"
