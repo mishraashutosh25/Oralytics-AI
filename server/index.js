5c81573554e7f05206bb3454b925c80a1df00848
ashutosh_mishra2812 <2k23.csaiml2313890@gmail.com>
2026-04-01 07:50:04 +0530

    Allow Vercel frontend in Backend CORS

--- server/index.js
+++ server/index.js
@@ -20,7 +20,8 @@ app.use(cors({
   origin: [
     "https://oralytics-ai-client.onrender.com",
     "http://localhost:5173",
-    "http://localhost:5174"
+    "http://localhost:5174",
+    "https://oralytics-ai.vercel.app"
   ],
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE"],
